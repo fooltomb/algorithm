fe4686bdcc42673c09b0936e7f19264bf2941257
fooltomb <fool_tomb@sina.com>
2018-11-16 16:14:44 +0800

    0053Done_Star

--- LeetCode/0053MaximumSubarray/maxSubarray_1.py
+++ LeetCode/0053MaximumSubarray/maxSubarray_1.py
@@ -1,3 +1,25 @@
 class Solution(object):
     def maxSubArray(self,nums):
-        
+        _sum=nums[0]
+        temp=nums[0]
+        for i in range(1,len(nums)):
+            temp=max(nums[i],nums[i]+temp)
+            _sum=max(_sum,temp)
+        return _sum
+
+if __name__=="__main__":
+    s=Solution()
+    print s.maxSubArray([3,-1,2])
+    print s.maxSubArray([-2,-1])
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
