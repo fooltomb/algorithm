8ef22beda5b4b48745c5f6ada12657dd0da4bd4b
fooltomb <fool_tomb@sina.com>
2018-11-21 15:25:26 +0800

    add myDataType

---
+++ LeetCode/DataType.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+#定义单链表节点
+class ListNode(object):
+    def __init__(self,x):
+        self.val=x
+        self.next=None
+#定义二叉树节点
+class TreeNode(object):
+    def __init__(self,x):
+        self.val=x
+        self.left=None
+        self.right=None
+
