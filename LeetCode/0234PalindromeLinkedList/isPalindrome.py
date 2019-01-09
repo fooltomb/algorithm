b1ccc4514e881dde42ed0cad11cce4c404083270
fooltomb <fool_tomb@sina.com>
2019-01-09 09:21:04 +0800

    0234Done

--- LeetCode/0234PalindromeLinkedList/isPalindrome.py
+++ LeetCode/0234PalindromeLinkedList/isPalindrome.py
@@ -1,25 +1,26 @@
 class Solution(object):
-    count=0
-    cur=head
-    while cur!=None:
-        count+=1
-        cur=cur.next
-    if count<2:
+    def isPanlindrome(self,head):
+        count=0
+        cur=head
+        while cur!=None:
+            count+=1
+            cur=cur.next
+        if count<2:
+            return True
+        cur=head
+        pre=None
+        for index in range(0,count):
+            if index<(count-1.1)/2:
+                head=cur
+                cur=cur.next
+                head.next=pre
+                pre=head
+            elif index>(count-0.9)/2:
+                if cur.val!=pre.val:
+                    return False
+                cur=cur.next
+                pre=pre.next
+            else:
+                cur=cur.next
         return True
-    cur=head
-    pre=None
-    for index in range(0,count):
-        if index<(count-1.1)/2:
-            head=cur
-            cur=cur.next
-            head.next=pre
-            pre=head
-        elif index>(count-0.9)/2:
-            if cur.val!=pre.val:
-                return False
-            cur=cur.next
-            pre=pre.next
-        else:
-            cur=cur.next
-    return True
 
