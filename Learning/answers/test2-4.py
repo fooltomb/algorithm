fd29c3814e76a9dc8484540e6aa4a33d89586a06
fooltomb <fool_tomb@sina.com>
2020-02-14 20:46:22 +0800

    fix test2-4

--- Learning/answers/test2-4.py
+++ Learning/answers/test2-4.py
@@ -1,6 +1,7 @@
 class Solution:
     def subsequence(self,n,m):
         s=0
+        if(n==0):return
         for i in range(n,m+1):
             s+=1.0/(i*i)
         print("%.4f"%s)
@@ -8,3 +9,5 @@ class Solution:
 if __name__=="__main__":
     s=Solution()
     s.subsequence(2,4)
+    s.subsequence(655,655360)
+    s.subsequence(0,0)
---
+++ Learning/problems_Chapter3.md
@@ -0,0 +1,6 @@
+#### 习题3-1 得分(Score,ACM/ICPC Seoul2005,UVa1585)
+
+​		给出一个由O和X组成的串(长度为1～80)，统计得分，每个O的得分为目前连续出现的O的个数，X的得分为0。例如，OOXXOXXOOO的得分为1+2+0+0+1+0+0+1+2+3。
+
+[答案](https://github.com/fooltomb/algorithm/blob/master/Learning/answers/test3-1.py)
+
